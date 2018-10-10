75a97885a82eab3d69c768ba1f80d1fdbf75367f
Noel Ryan <noelgerard.ryan@gmail.com>
2018-10-10 13:24:58 +0200

    rspec tests passing had issue with rails helper fixed

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,4 +1,3 @@
-require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 
@@ -6,9 +5,9 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require 'spec_helper'
 require 'rspec/rails'
 
-  ActiveRecord::Migration.maintain_test_schema!
+ActiveRecord::Migration.maintain_test_schema!
 
-  Dir[Rails.root.join('spec/support/**/*:rb')].each { |f| require f }
+Dir[Rails.root.join('spec/support/**/*rb')].each { |f| require f }
   
 RSpec.configure do |config|
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
@@ -16,3 +15,4 @@ RSpec.configure do |config|
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
 end
+
